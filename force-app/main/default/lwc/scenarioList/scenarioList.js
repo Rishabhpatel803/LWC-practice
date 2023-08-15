a832ba8410423a95383dfd1b1abb30940974d98a
Rishabhpatel803 <rockrishabh456@gmail.com>
2023-08-15 22:58:23 +0530

    Created the Component with the link of scenarios and codes of the implemented scenario

---
+++ force-app/main/default/lwc/scenarioList/scenarioList.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class ScenarioList extends LightningElement {
+    urlScenario3 = 'https://salesforcediaries.com/2022/09/17/lwc-exchange-scenario-3';
+    urlSolution3Parent = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/lES3Parent';
+    urlSolution3Child = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/lES3Child';
+
+    urlScenario6 = 'https://salesforcediaries.com/2022/10/01/lwc-exchange-scenario-6';
+    urlSolution6 = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/lWCExchangeScenario6';
+    urlApex6 = 'https://github.com/Rishabhpatel803/LWC-practice/blob/master/force-app/main/default/classes/getDetails.cls';
+
+    accountSearchLWC = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/accountSearch';
+    accountSearchApex = 'https://github.com/Rishabhpatel803/LWC-practice/blob/master/force-app/main/default/classes/ReusableLookupController.cls';
+}
