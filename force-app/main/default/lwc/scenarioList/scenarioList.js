5e48e2c1e15ce84353e139c3f6edf0342a797892
Rishabh Patel <56493772+Rishabhpatel803@users.noreply.github.com>
2023-08-21 16:23:00 +0530

    Update scenarioList.js

--- force-app/main/default/lwc/scenarioList/scenarioList.js
+++ force-app/main/default/lwc/scenarioList/scenarioList.js
@@ -1,9 +1,18 @@
 import { LightningElement } from 'lwc';
 
 export default class ScenarioList extends LightningElement {
+
+    urlScenario2 = 'https://salesforcediaries.com/2022/09/17/lwc-exchange-scenario-2';
+    urlSolution2Parent = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/scenarioTwoParent';
+    urlSolution2Child = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/scenarioTwoChild';
+
     urlScenario3 = 'https://salesforcediaries.com/2022/09/17/lwc-exchange-scenario-3';
     urlSolution3Parent = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/lES3Parent';
     urlSolution3Child = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/lES3Child';
+
+    urlScenario4 = 'https://salesforcediaries.com/2022/09/20/lwc-exchange-scenario-4';
+    urlSolution4 = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/scenarioFour';
+    urlSolution4Service = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/scenarioFourService';
 
     urlScenario6 = 'https://salesforcediaries.com/2022/10/01/lwc-exchange-scenario-6';
     urlSolution6 = 'https://github.com/Rishabhpatel803/LWC-practice/tree/master/force-app/main/default/lwc/lWCExchangeScenario6';
